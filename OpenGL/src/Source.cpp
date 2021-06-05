18445e0fe6e840502f23d15952f297e98a0c6d67
Basil Malayamma <basilmalayamma@gmail.com>
2021-06-05 20:59:38 +0530

    Modify the shader to read from file.

--- OpenGL/src/Source.cpp
+++ OpenGL/src/Source.cpp
@@ -1,6 +1,60 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+class shaderSource {
+public:
+    shaderSource(std::string vertex, std::string fragment) {
+        vs = vertex;
+        fs = fragment;
+    }
+
+    std::string vs;
+    std::string fs;
+};
+
+shaderSource getShader(std::string file_path) {
+    std::ifstream file;
+    std::string line;
+
+    enum shaderType {
+        Vertex = 0,
+        Fragment = 1
+    };
+
+    file.open(file_path);
+    int mode = -1;
+    std::stringstream ss[2];
+
+    while (getline(file, line)) {
+        if ((line.find("shader")) != std::string::npos) {
+            if ((line.find("vertex")) != std::string::npos) {
+                mode = Vertex;
+                continue;
+            }
+            else if ((line.find("fragment")) != std::string::npos) {
+                mode = Fragment;
+                continue;
+            }       
+        }
+        ss[mode] << line << "\n";
+    }
+
+    /*
+    std::cout << "Vertex shader: " << std::endl;
+    std::cout << ss[0].str() << std::endl;
+
+    std::cout << "Fragment shader: " << std::endl;
+    std::cout << ss[1].str() << std::endl;
+    */
+
+    shaderSource shaderSrc(ss[Vertex].str(), ss[Fragment].str());
+    return shaderSrc;
+
+}
 
 unsigned int compileShader(std::string &source, GLenum type) {
     unsigned int shader = glCreateShader(type);
@@ -75,37 +129,23 @@ int main(void)
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
 
-    std::string vs =
-        "#version 330 core\n"
-        "layout(location = 0) in vec4 position;\n"
-        "void main()\n"
-        "{\n"
-        "gl_Position = position;\n"
-        "}\n";
+    shaderSource shaderSrc = getShader("resource/shader/basic.shader");
 
-    std::string fs =
-        "#version 330 core\n"
-        "layout(location = 0) out vec4 colour;\n"
-        "void main()\n"
-        "{\n"
-        "colour = vec4(1.0, 0.0, 0.0, 1.0);\n"
-        "}\n";
-
-    unsigned int program = createProgram(vs, fs);
+    unsigned int program = createProgram(shaderSrc.vs, shaderSrc.fs);
     glUseProgram(program);
         
-    /* Loop until the user closes the window */
+    //Loop until the user closes the window
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
+        //Render here
         glClear(GL_COLOR_BUFFER_BIT);
 
         glDrawArrays(GL_TRIANGLES, 0, 6 * sizeof(float));
 
-        /* Swap front and back buffers */
+        //Swap front and back buffers
         glfwSwapBuffers(window);
 
-        /* Poll for and process events */
+        //Poll for and process events
         glfwPollEvents();
     }
 
